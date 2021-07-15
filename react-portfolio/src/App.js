7cfb44bafea9eb402f7953fbfd94568a946493de
Beau4ta <beaufortier13@gmail.com>
2021-07-15 14:12:50 -0400

    ereadme

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -15,7 +15,7 @@ function App() {
     <div className="App">
      <Header />
      <Navbar />
-     <Route exact path ='/about' component={About} />
+     <Route exact path ='/' component={About} />
      <Route exact path='/projects' component={Projects} />
      <Route exact path='/resume' component={Resume} />
      <Route exact path='/contact' component={Contact} />
